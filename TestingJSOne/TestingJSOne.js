d30ccc646cc0b9a4e5b52f77f00f32ed374bb618
Bala <c.durai.delhi@gmail.com>
2023-10-02 12:03:13 +0530

    Added 2 more variables using var keyword!

--- TestingJSOne/TestingJSOne.js
+++ TestingJSOne/TestingJSOne.js
@@ -1,2 +1,6 @@
-var valueOne = 10;
+var valueOne  = 10;
+var valueTwo  = 20;
+var endResult = valueOne + valueTwo;
 console.log("Value One is = ", valueOne);
+console.log("Value Two is = ", valueTwo);
+console.log("End Result is = ", endResult);
